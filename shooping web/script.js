90bc76680d34ef0563125a861a3c0ab3617132ea
Muzaffar Ameen <muzaffarameen389@gmail.com>
2025-10-22 21:35:30 +0500

    Add files via upload

---
+++ shooping web/script.js
@@ -0,0 +1,17 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//   const text = "Welcome to our website";
+//   const typingTextElement = document.getElementById('typing-text');
+//   let index = 0;
+
+//   function type() {
+//     if (index < text.length) {
+//       typingTextElement.textContent += text.charAt(index);
+//       index++;
+//       setTimeout(type, 100); // Adjust typing speed here
+//     } else {
+//       typingTextElement.style.borderRight = 'none'; // Remove cursor after typing
+//     }
+//   }
+
+//   type();
+// });
